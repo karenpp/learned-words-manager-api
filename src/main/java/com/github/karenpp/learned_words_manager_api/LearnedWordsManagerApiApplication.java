45db82d4d07b77867ed3ce0da057b383aabae645
Karenpp <karenpaulapinto@gmail.com>
2021-07-12 00:29:02 -0300

    Initializing repository

---
+++ src/main/java/com/github/karenpp/learned_words_manager_api/LearnedWordsManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.karenpp.learned_words_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnedWordsManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnedWordsManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/karenpp/learned_words_manager_api/LearnedWordsManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.karenpp.learned_words_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnedWordsManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
